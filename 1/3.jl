085008a3d583bf861345926790fb119c1228784e
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-11-17 15:12:34 +0300

    3.jl

--- 1/3.jl
+++ 1/3.jl
@@ -1,4 +1,4 @@
-function invmod1(a::T, M::T) where T
+function invmod1(a::T, M::T) where T   #Эта функция вычисляет обратный элемент a^(-1) mod M в кольце по модулю M, используя расширенный алгоритм Евклида.
     d, x, y = extended_euclidean_algorithm(a, M)
     if d == 1
         return mod(x, M)
