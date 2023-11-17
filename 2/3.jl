8ef981f28daa21059363be9b43caf925b33771c5
otletevshiy <113855826+otletevshiy@users.noreply.github.com>
2023-11-17 15:03:50 +0300

    3.jl

--- 2/3.jl
+++ 2/3.jl
@@ -1,4 +1,4 @@
-function log_2(n)
+function log_2(n)   #Эта функция использует интерактивный метод для нахождения логарифма по основанию 2.
     log_Value = -1
     while (n > 0)
         log_Value = log_Value + 1
@@ -7,6 +7,6 @@ function log_2(n)
     return log_Value
 end
 
-function log(a::Int64, b::Int64)
+function log(a::Int64, b::Int64)   #Эта функция использует log_2 для нахождения  логарифма числа b по основанию 2 и делит его на логарифм числа a по основанию 2.
     return log_2(b) / log_2(a)
 end
